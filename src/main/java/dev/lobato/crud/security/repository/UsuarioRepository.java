46a99e5e3fe20e61c405fd298e8e1199974faaf1
Carlos Lobato <lobatocarlos799@gmail.com>
2023-08-31 16:15:47 -0600

    new : se creo la interface con jpa para usuario

---
+++ src/main/java/dev/lobato/crud/security/repository/UsuarioRepository.java
@@ -0,0 +1,22 @@
+package dev.lobato.crud.security.repository;
+
+
+import dev.lobato.crud.security.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+
+    Optional<Usuario> findByNombreUsuario(String nombreUsuario);
+
+    boolean existsByNombreUsuario(String nombreUsuario);
+    boolean existsByEmail(String email);
+
+
+
+
+}
