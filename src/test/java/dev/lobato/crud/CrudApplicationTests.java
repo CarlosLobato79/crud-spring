c9c4d10ec807c87918bd88b12d2fd627f6cc1918
Carlos Lobato <lobatocarlos799@gmail.com>
2023-08-31 15:04:23 -0600

    new: se creo la estructura del proyecto

---
+++ src/test/java/dev/lobato/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lobato.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
