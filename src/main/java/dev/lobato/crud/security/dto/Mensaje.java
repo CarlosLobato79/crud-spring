13c64e4be279934d0bb52bec68ef029b477b92b5
Carlos Lobato <lobatocarlos799@gmail.com>
2023-08-31 18:08:56 -0600

    new: se creo el dto para mensaje

---
+++ src/main/java/dev/lobato/crud/security/dto/Mensaje.java
@@ -0,0 +1,12 @@
+package dev.lobato.crud.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Mensaje {
+    private String mensaje;
+}
