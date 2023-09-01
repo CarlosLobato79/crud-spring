f3bafdf1247b2af4ae432d06ce2a99f637f12dcf
Carlos Lobato <lobatocarlos799@gmail.com>
2023-08-31 18:29:12 -0600

    new: se creo el servicio para el usuario

---
+++ src/main/java/dev/lobato/crud/security/service/UsuarioService.java
@@ -0,0 +1,34 @@
+package dev.lobato.crud.security.service;
+
+
+import dev.lobato.crud.security.entity.Usuario;
+import dev.lobato.crud.security.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UsuarioService {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    public Optional<Usuario> getByNombreUsuario(String nombreUsuario){
+        return usuarioRepository.findByNombreUsuario(nombreUsuario);
+    }
+
+    public boolean existsByNOmbreUsuario(String nombreUsuario){
+        return usuarioRepository.existsByNombreUsuario(nombreUsuario);
+    }
+
+    public boolean existsByEmail(String email){
+        return usuarioRepository.existsByEmail(email);
+    }
+
+    public void save(Usuario usuario){
+        usuarioRepository.save(usuario);
+    }
+}
