40b620e422f5e3ba8d1bba9541e0c285b98a016b
Carlos Lobato <lobatocarlos799@gmail.com>
2023-08-31 18:29:31 -0600

    new: se creo el servicio de Producto

---
+++ src/main/java/dev/lobato/crud/service/ProductoService.java
@@ -0,0 +1,43 @@
+package dev.lobato.crud.service;
+
+import dev.lobato.crud.entity.Producto;
+import dev.lobato.crud.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class ProductoService {
+
+    @Autowired
+    ProductoRepository productoRepository;
+
+
+    public List<Producto> list(){
+        return productoRepository.findAll();
+    }
+
+    public Optional<Producto> getOne(int id){
+        return productoRepository.findById(id);
+    }
+
+    public void save(Producto producto){
+        productoRepository.save(producto);
+    }
+
+    public void delete(int id){
+        productoRepository.deleteById(id);
+    }
+
+    public boolean existsById(int id){
+        return productoRepository.existsById(id);
+    }
+
+    public boolean existsByNombre(String nombre){
+        return productoRepository.existsByNombre(nombre);
+    }
+}
