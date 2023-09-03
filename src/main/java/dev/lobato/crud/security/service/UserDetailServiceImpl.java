76edb1f077abb676e5800aee0c14be0cc698cea7
Carlos Lobato <lobatocarlos799@gmail.com>
2023-09-03 12:52:50 -0600

    new: se creo la implementacio para cargar el usuario por el nombre

---
+++ src/main/java/dev/lobato/crud/security/service/UserDetailServiceImpl.java
@@ -0,0 +1,20 @@
+package dev.lobato.crud.security.service;
+
+import dev.lobato.crud.security.entity.Usuario;
+import dev.lobato.crud.security.entity.UsuarioPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UsuarioService usuarioService;
+
+    @Override
+    public UserDetails loadUserByUsername(String nombreUsuario) throws UsernameNotFoundException {
+        Usuario usuario = usuarioService.getByNombreUsuario(nombreUsuario).get();
+        return UsuarioPrincipal.build(usuario);
+    }
+}
